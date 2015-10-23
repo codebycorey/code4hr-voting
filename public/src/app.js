9e5be08ab91d961b3b22eb48ff6f6f011930ee94
Mark Carter <mark4carter@gmail.com>
2015-10-23 16:16:55 -0400

    buttons vote buttons

--- public/src/app.js
+++ public/src/app.js
@@ -16,8 +16,40 @@ app.factory('DataService', function($q, $http) {
 });
 
 app.controller('MainController', function($scope, DataService) {
+  $scope.voted = 'none';
+
   DataService.getData().then(function(data){
     $scope.candidates = data.candidates;
     console.log(data.candidates);
   });
+
+  $scope.changeVote = function(flag, iss) {
+
+    if ($scope.voted == 'none') {
+      if (flag == 'up') {
+        iss.up++;
+      } else {
+        iss.down++;
+      }
+      $scope.voted = flag;
+    } else {
+      if ($scope.voted == 'up' && flag == 'up') {
+        iss.up--;
+        $scope.voted = 'none';
+      } else if ($scope.voted == 'up' && flag == 'down') {
+        iss.up--;
+        iss.down++;
+        $scope.voted = flag;
+      } else if ($scope.voted == 'down' && flag == 'down') {
+        iss.down--;
+        $scope.voted = 'none';
+      } else if ($scope.voted == 'down' && flag == 'up') {
+        iss.up++;
+        iss.down--;
+        $scope.voted = flag;
+      }
+    }
+
+  }
+
 });
