309a8dc9606215fc348bfda7a4ddb80ff1ffba8e
Joshua Harris <joshua.jharris13@gmail.com>
2015-10-24 10:35:25 -0400

    added address autocomplete

--- public/src/app.js
+++ public/src/app.js
@@ -15,10 +15,29 @@ app.factory('DataService', function($q, $http) {
             return defer.promise;
         }
     };
+});
 
+app.directive('googleplace', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {}
+            };
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                scope.$apply(function() {
+                    model.$setViewValue(element.val());
+                });
+            });
+        }
+    };
 });
 
 app.controller('MainController', function($scope, DataService) {
+    $scope.gPlace;
 
     //$scope.address = '';
     $scope.address = '';
