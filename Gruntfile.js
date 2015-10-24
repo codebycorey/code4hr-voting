77df04f334870986b4e17ba747ccf3d3c066f9e4
FVCproductions <fvcproductions@icloud.com>
2015-10-24 19:50:19 -0400

    Made some path changes to work with Gitub pages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,16 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		// compass: {
-		// 	dist: {
-		// 		options: {
-		// 			sassDir: 'sass',
-		// 			cssDir: 'css',
-		// 			outputStyle: 'compressed'
-		// 		}
-		// 	}
-		// },
+    grunt.initConfig({
         connect: {
-          server: {
-            options: {
-              port: 9001,
-              base: 'public',
-              keepalive: true
+            server: {
+                options: {
+                    port: 9001,
+                    base: 'public',
+                    keepalive: true
+                }
             }
-          }
         },
-		watch: {
-			// css: {
-			// 	files: ['sass/*.scss'],
-			// 	tasks: ['compass']
-			// }
-		}
-	});
-
-	// grunt.loadNpmTasks('grunt-contrib-compass');
+    });
     grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['connect']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['connect']);
 };
