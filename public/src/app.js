e170be3dbbc9492cbdf9b53135b8294e64d39b77
FVCproductions <fvcproductions@icloud.com>
2015-10-23 23:04:42 -0400

    Some visual changes.

--- public/src/app.js
+++ public/src/app.js
@@ -14,7 +14,7 @@ app.factory('DataService', function($q, $http) {
 
             return defer.promise;
         }
-    }
+    };
 
 });
 
@@ -29,74 +29,74 @@ app.controller('MainController', function($scope, DataService) {
             $scope.civicInfo = data;
             console.log(data);
         });
+    };
+});
+
+/*
+    $scope.address = '';
+
+  //$scope.voted = 'none';
+  $scope.voted = {};
+
+
+  DataService.getData().then(function(data){
+    $scope.candidates = data.candidates;
+    $scope.candidates.forEach(function(element, index){
+      element.index = index;
+    });
+  });
+
+  $scope.changeVote = function(flag, iss, cName, ina) {
+    names = cName + ina + iss.description;
+    voted = "none";
+    console.log(ina);
+
+    if (names in $scope.voted) {
+      voted = $scope.voted[names];
+    } else {
+      if (flag == 'up') {
+        $scope.voted[names] = 'up';
+      } else {
+        $scope.voted[names] = 'down';
+      }
     }
 
-    /*
-        $scope.address = '';
-
-      //$scope.voted = 'none';
-      $scope.voted = {};
-
-
-      DataService.getData().then(function(data){
-        $scope.candidates = data.candidates;
-        $scope.candidates.forEach(function(element, index){
-          element.index = index;
-        });
-      });
-
-      $scope.changeVote = function(flag, iss, cName, ina) {
-        names = cName + ina + iss.description;
-        voted = "none";
-        console.log(ina);
-
-        if (names in $scope.voted) {
-          voted = $scope.voted[names];
-        } else {
-          if (flag == 'up') {
-            $scope.voted[names] = 'up';
-          } else {
-            $scope.voted[names] = 'down';
-          }
-        }
-
-        if (voted == 'none') {
-          if (flag == 'up') {
-            iss.up++;
-          } else {
-            iss.down++;
-          }
-          $scope.voted[names] = flag;
-        } else {
-          if (voted == 'up' && flag == 'up') {
-            iss.up--;
-            $scope.voted[names] = 'none';
-          } else if (voted == 'up' && flag == 'down') {
-            iss.up--;
-            iss.down++;
-            $scope.voted[names] = flag;
-          } else if (voted == 'down' && flag == 'down') {
-            iss.down--;
-            $scope.voted[names] = 'none';
-          } else if (voted == 'down' && flag == 'up') {
-            iss.up++;
-            iss.down--;
-            $scope.voted[names] = flag;
-          }
-        }
-
+    if (voted == 'none') {
+      if (flag == 'up') {
+        iss.up++;
+      } else {
+        iss.down++;
       }
-
-      $scope.newIssue = {};
-
-      $scope.addNewIssue = function($event, index) {
-        $scope.newIssue.up = 0;
-        $scope.newIssue.down = 0;
-        console.log($scope.candidates);
-        console.log(index);
-        $scope.candidates[index].issues.push($scope.newIssue);
-        $scope.newIssue = {};
-        $event.preventDefault();
+      $scope.voted[names] = flag;
+    } else {
+      if (voted == 'up' && flag == 'up') {
+        iss.up--;
+        $scope.voted[names] = 'none';
+      } else if (voted == 'up' && flag == 'down') {
+        iss.up--;
+        iss.down++;
+        $scope.voted[names] = flag;
+      } else if (voted == 'down' && flag == 'down') {
+        iss.down--;
+        $scope.voted[names] = 'none';
+      } else if (voted == 'down' && flag == 'up') {
+        iss.up++;
+        iss.down--;
+        $scope.voted[names] = flag;
       }
-      */
-});
+    }
+
+  }
+
+  $scope.newIssue = {};
+
+  $scope.addNewIssue = function($event, index) {
+    $scope.newIssue.up = 0;
+    $scope.newIssue.down = 0;
+    console.log($scope.candidates);
+    console.log(index);
+    $scope.candidates[index].issues.push($scope.newIssue);
+    $scope.newIssue = {};
+    $event.preventDefault();
+  }
+  */
